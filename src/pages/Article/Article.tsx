c0d95a3d3ab3e6af4418d52303bbb4f4873f9c20
Kadu <ce.magalhaespereira@gmail.com>
2023-01-27 19:35:36 -0300

    Add article info in Article component

--- src/pages/Article/Article.tsx
+++ src/pages/Article/Article.tsx
@@ -1,26 +1,85 @@
 import { Container } from './styles'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import {
+  faUpRightFromSquare,
+  faCalendarDay,
+  faComment,
+  faChevronLeft,
+} from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { Link } from 'react-router-dom'
 
 export function Article() {
   return (
     <Container>
       <div className="article-info">
         <div className="top">
-          <a href="/" className="back-anchor">
-            Voltar
-          </a>
-          <a href="/" className="github-link" target="_blank" rel="noreferrer">
-            <span className="link-text">GITHUB</span>
+          <Link to="/" className="back-link link">
+            <FontAwesomeIcon
+              icon={faChevronLeft}
+              color="#3294F8"
+              className="github-icon icon"
+            />
+            <span className="link-text">Voltar</span>
+          </Link>
+          <a
+            href="/"
+            className="github-link link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="link-text">Ver no Github</span>
             <FontAwesomeIcon
               icon={faUpRightFromSquare}
               color="#3294F8"
-              className="github-icon"
+              className="github-icon icon"
             />
           </a>
         </div>
+        <h2 className="article-title">
+          JavaScript data types and data structures
+        </h2>
+        <ul className="data-list">
+          <li>
+            <FontAwesomeIcon
+              className="data-list-icon"
+              icon={faGithub as IconProp}
+              color="#3A536B"
+            />
+            kdumagalhaes
+          </li>
+          <li>
+            <FontAwesomeIcon
+              icon={faCalendarDay}
+              color="#3A536B"
+              className="calendar-icon"
+            />
+            Há 1 dia
+          </li>
+          <li>
+            <FontAwesomeIcon
+              icon={faComment}
+              color="#3A536B"
+              className="comment-icon"
+            />
+            5 comentários
+          </li>
+        </ul>
       </div>
       <article className="article-content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+        voluptates nisi ex nihil impedit laboriosam inventore porro dolorum rem
+        at consequuntur neque ipsum, ipsa quam quaerat incidunt optio qui animi?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+        voluptates nisi ex nihil impedit laboriosam inventore porro dolorum rem
+        at consequuntur neque ipsum, ipsa quam quaerat incidunt optio qui animi?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+        voluptates nisi ex nihil impedit laboriosam inventore porro dolorum rem
+        at consequuntur neque ipsum, ipsa quam quaerat incidunt optio qui animi?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+        voluptates nisi ex nihil impedit laboriosam inventore porro dolorum rem
+        at consequuntur neque ipsum, ipsa quam quaerat incidunt optio qui animi?
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
         voluptates nisi ex nihil impedit laboriosam inventore porro dolorum rem
         at consequuntur neque ipsum, ipsa quam quaerat incidunt optio qui animi?
--- src/pages/Article/styles.tsx
+++ src/pages/Article/styles.tsx
@@ -1,3 +1,78 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  .article-info {
+    max-width: 54rem;
+    margin: -90px auto 0 auto;
+    background-color: ${({ theme }) => theme['base-profile']};
+    border-radius: 10px;
+    padding: 2rem 2.5rem;
+    display: flex;
+    flex-direction: column;
+
+    .top,
+    .data-list {
+      display: flex;
+    }
+    .top {
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 20px;
+    }
+
+    .article-title {
+      font-size: 1.5rem;
+      margin-bottom: 10px;
+    }
+
+    .link {
+      text-decoration: none;
+      display: flex;
+      align-items: center;
+      gap: 7px;
+      font-weight: 700;
+      transition: all 0.3s;
+
+      &:hover {
+        border-bottom: 2px solid ${({ theme }) => theme.blue};
+      }
+    }
+
+    .icon {
+      font-size: 0.75rem;
+    }
+
+    .back-link {
+      margin-left: 0.6px;
+    }
+
+    .link-text {
+      color: ${({ theme }) => theme.blue};
+      font-size: 0.75rem;
+      text-transform: uppercase;
+    }
+
+    .data-list {
+      gap: 2rem;
+      list-style: none;
+
+      li {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        color: ${({ theme }) => theme['base-span']};
+      }
+
+      .data-list-icon {
+        height: 1.125rem;
+        width: 1.125rem;
+      }
+    }
+  }
+
+  .article-content {
+    max-width: 54rem;
+    padding: 2.5rem 2rem;
+    margin: 0 auto 8rem auto;
+  }
+`
